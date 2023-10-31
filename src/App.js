777c197e24aaa106144a8a09fb7bba533677c65b
Rakshit Singh <89170079+Bot-Rakshit@users.noreply.github.com>
2023-10-31 11:41:31 +0530

    Update App.js
    Result changes updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { Toolbar, TextField, Button, Container, Box, Select, MenuItem } from '@mui/material';
+import { Toolbar, Button, Container, Box, ButtonGroup} from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Chess } from 'chess.js';
 import EvalBar from './evalbar';
 import css from './App.css';
+import TournamentsList from './TournamentsList';
+import { WidthFull } from '@mui/icons-material';
 
 const theme = createTheme({
   palette: {
@@ -12,7 +14,7 @@ const theme = createTheme({
       default: 'transparent',
     },
     primary: {
-      main: '#fcbcd5',
+      main: '#ADD8E6',
     },
     secondary: {
       main: '#b9bbce',
@@ -30,35 +32,77 @@ const theme = createTheme({
     '"Segoe UI Emoji"',
     '"Segoe UI Symbol"',
   ].join(','),
-},
-);
+});
 
+const GameCard = ({ game, onClick, isSelected }) => (
+  <Button 
+      variant={isSelected ? "contained" : "outlined"} 
+      color={isSelected ? "primary" : "secondary"} 
+      style={{
+          margin: '2px',
+          padding: '6px',
+          fontSize: '0.8em',
+          fontWeight: 'bold',
+          boxShadow: isSelected ? '0px 0px 12px 2px rgba(252,188,213,0.6)' : 'none'
+      }}
+      onClick={onClick}
+  >
+      {game}
+  </Button>
+);
 function App() {
-  const [broadcastID, setBroadcastID] = useState('');
+  const [broadcastIDs, setBroadcastIDs] = useState([]);
   const [isBroadcastLoaded, setIsBroadcastLoaded] = useState(false);
   const [links, setLinks] = useState([]);
   const [availableGames, setAvailableGames] = useState([]);
-  const [selectedGame, setSelectedGame] = useState(null);
+  const [selectedGames, setSelectedGames] = useState([]);
+  const [background, setBackground] = useState('chroma');
+  const [isChromaBackground, setIsChromaBackground] = useState(true);
+  const [prevEvals, setPrevEvals] = useState({});
+  const [layout, setLayout] = useState('grid'); // default layout is 'row'
 
   const allGames = useRef("");
-  const abortController = useRef(null);
+  const abortControllers = useRef({});
 
   const fetchEvaluation = async (fen) => {
-    const endpoint = `http://"IP(of the flask server)"/evaluate?fen=${encodeURIComponent(fen)}`;
+    const endpoint = `http://139.59.79.118:5000/evaluate?fen=${encodeURIComponent(fen)}`;
     const response = await fetch(endpoint, { method: 'GET', mode: 'cors' });
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
     return await response.json();
   };
+ // default to dark background
+ const handleRemoveLink = (index) => {
+  setLinks(prevLinks => prevLinks.filter((link, i) => i !== index));
+};
 
-  const startStreaming = async () => {
-    if (abortController.current) abortController.current.abort();
-    abortController.current = new AbortController();
+const toggleBackground = () => {
+  if (background === 'chroma') {
+    document.body.classList.remove('chroma-background');
+    document.body.classList.add('dark-background');
+    setBackground('dark');
+  } else {
+    document.body.classList.remove('dark-background');
+    document.body.classList.add('chroma-background');
+    setBackground('chroma');
+  }
+};
 
-    const streamURL = `https://lichess.org/api/stream/broadcast/round/${broadcastID.split('/').pop()}.pgn`;
-    const response = await fetch(streamURL, { signal: abortController.current.signal });
 
+  const handleTournamentSelection = async (tournamentIds) => {
+    console.log("Received Tournament IDs:", tournamentIds); 
+    setIsBroadcastLoaded(true);
+    tournamentIds.forEach(tournamentId => startStreaming(tournamentId));
+  };
+
+  const startStreaming = async (tournamentId) => {
+    if (abortControllers.current[tournamentId]) abortControllers.current[tournamentId].abort();
+    abortControllers.current[tournamentId] = new AbortController();
+  
+    const streamURL = `https://lichess.org/api/stream/broadcast/round/${tournamentId}.pgn`;
+    const response = await fetch(streamURL, { signal: abortControllers.current[tournamentId].signal });
+    console.log("Stream URL:", streamURL);
     const reader = response.body.getReader();
 
     const processStream = async () => {
@@ -68,17 +112,17 @@ function App() {
       allGames.current += new TextDecoder().decode(value);
       updateEvaluations();
       fetchAvailableGames();
-      setTimeout(processStream, 1000); // Re-check for updates every second
+      setTimeout(processStream, 10);
     };
     processStream();
   };
 
   const handleCloseConnection = () => {
-    if (abortController.current) abortController.current.abort();
+    Object.values(abortControllers.current).forEach(controller => controller.abort());
+    abortControllers.current = {};
     setIsBroadcastLoaded(false);
-    setBroadcastID('');
+    setBroadcastIDs([]);
   };
-
   const fetchAvailableGames = () => {
     const games = allGames.current.split("\n\n\n");
     const gameOptions = games.map(game => {
@@ -89,20 +133,24 @@ function App() {
     setAvailableGames(Array.from(new Set(gameOptions)));
   };
 
-  const handleGameSelection = (event) => {
-    setSelectedGame(event.target.value);
-  };
-
-  const addSelectedGame = () => {
-    if (selectedGame) {
-      const [whitePlayer, blackPlayer] = selectedGame.split(' - ');
-      if (!links.some(link => link.whitePlayer === whitePlayer && link.blackPlayer === blackPlayer)) {
-        setLinks([...links, { evaluation: null, whitePlayer, blackPlayer, error: null, lastFEN: '' }]);
-        updateEvaluationsForLink({ whitePlayer, blackPlayer });
-      }
+  const handleGameSelection = (game) => {
+    if (selectedGames.includes(game)) {
+        setSelectedGames(prevGames => prevGames.filter(g => g !== game));
+    } else {
+        setSelectedGames(prevGames => [...prevGames, game]);
     }
   };
 
+  const addSelectedGames = () => {
+    for (let game of selectedGames) {
+      const [whitePlayer, blackPlayer] = game.split(' - ');
+      if (!links.some(link => link.whitePlayer === whitePlayer && link.blackPlayer === blackPlayer)) {
+        setLinks(prevLinks => [...prevLinks, { evaluation: null, whitePlayer, blackPlayer, error: null, lastFEN: '' }]);
+        updateEvaluationsForLink({ whitePlayer, blackPlayer });
+      }
+    }
+    setSelectedGames([]); // Clear selected games after adding
+  };
   const updateEvaluationsForLink = async (link) => {
     const games = allGames.current.split("\n\n\n");
     const specificGamePgn = games.reverse().find(game => {
@@ -134,7 +182,7 @@ function App() {
          else if (result === "0-1") gameResult = `Bravo!! ${formatName(link.blackPlayer)}!`;
          else if (result === "1/2-1/2") gameResult = "It's a draw! 🤝";
 }
-
+        
 
       
     
@@ -146,6 +194,9 @@ function App() {
           const currentFEN = chess.fen();
 
           if (currentFEN !== link.lastFEN || gameResult !== link.result) {
+            const evalData = await fetchEvaluation(currentFEN);
+    const prevEval = prevEvals[link.whitePlayer + link.blackPlayer];
+
 
               try {
                   const evalData = await fetchEvaluation(currentFEN);
@@ -188,7 +239,7 @@ function App() {
       await updateEvaluationsForLink(link);
     }
   };
-
+  
   useEffect(() => {
     if (links.length) {
       const interval = setInterval(() => {
@@ -197,6 +248,16 @@ function App() {
       return () => clearInterval(interval);  // Cleanup on component unmount or if links change
     }
   }, [links]);
+  useEffect(() => {
+    if (isChromaBackground) {
+      document.body.classList.add('chroma-background');
+      document.body.classList.remove('dark-background');
+    } else {
+      document.body.classList.remove('chroma-background');
+      document.body.classList.add('dark-background');
+    }
+  }, [isChromaBackground]);
+
 
 
 
@@ -205,81 +266,73 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="md">
-          <Toolbar>
-            <img src="https://i.imgur.com/z2fbMtT.png" alt="ChessBase India Logo" style={{ height: '100px' }} />
-          </Toolbar>
-
-        <Box mt={4} px={3}
-          sx={{
-            backgroundColor: 'rgba(1, 1, 1, 0.6)',
-            padding: 2,
-            borderRadius: 2,
-            marginBottom: 2
-          }}>
-          <TextField
-            fullWidth
-            variant="outlined"
-            label="Broadcast ID"
-            value={broadcastID}
-            disabled={isBroadcastLoaded}
-            onChange={(e) => setBroadcastID(e.target.value)}
-          />
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginTop: '10px' }}
-            disabled={isBroadcastLoaded}
-            onClick={() => {
-              setIsBroadcastLoaded(true);
-              startStreaming();
-            }}
-          >
-            Load Broadcast
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            style={{ marginTop: '10px' }}
-            onClick={handleCloseConnection}
-          >
-            Close Connection
-          </Button>
-          <Select
-            fullWidth
-            value={selectedGame}
-            onChange={handleGameSelection}
-            sx={{ marginTop: 2 }}
-          >
-            {availableGames.map((game, index) => (
-              <MenuItem key={index} value={game}>
-                {game}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button
-            variant="contained"
-            color="primary"
-            style={{ marginTop: '10px' }}
-            onClick={addSelectedGame}
-          >
-            Add Selected Game Bar
-          </Button>
-        </Box>
-        <Box mt={4} px={3} display="flex" flexDirection="column" alignItems="center">
-          {links.map((link, index) => (
+        <Container maxWidth="md" className={isChromaBackground ? 'chroma-background' : 'dark-background'}>
+        <Toolbar>
+    <Box style={{ display: 'flex', justifyContent: 'center', flexGrow: 1.5 }}>
+        <img src="https://i.imgur.com/z2fbMtT.png" alt="ChessBase India Logo" style={{ height: '100px', marginTop: '20px' }} />
+    </Box>
+    <Button 
+        variant="contained" 
+        onClick={toggleBackground}
+    >
+        BG
+    </Button>
+</Toolbar>
+            
+            {isBroadcastLoaded ? (
+                <Box mt={4} px={3}
+                    sx={{
+                        backgroundColor: 'rgba(1, 1, 1, 0.6)',
+                        padding: 2,
+                        borderRadius: 2,
+                        marginBottom: 2
+                    }}>
+                    {availableGames.map((game, index) => (
+                        <GameCard 
+                            key={index} 
+                            game={game} 
+                            onClick={() => handleGameSelection(game)}
+                            isSelected={selectedGames.includes(game)}
+                        />
+                    ))}
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        style={{ marginTop: '10px' }}
+                        onClick={addSelectedGames}
+                    >
+                        Add Selected Games Bar
+                    </Button>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        style={{ marginTop: '10px' }}
+                        onClick={handleCloseConnection}
+                    >
+                        Close Connection
+                    </Button>
+                </Box>
+            ) : (
+                <TournamentsList onSelect={handleTournamentSelection} />
+            )}
+            
+            <Box mt={4} px={3} className="eval-bars-container">
+    {links.map((link, index) => (
+      <Box display="flex" flexDirection="row" alignItems="flex-start" justifyContent="center" style={{ marginBottom: '0px' }}>
+            <Button className="close-button" onClick={() => handleRemoveLink(index)}>Close</Button>
             <EvalBar
-            key={`${link.whitePlayer}-${link.blackPlayer}-${link.result}`} // Changing the key when the result changes
-            evaluation={link.evaluation}
-            whitePlayer={link.whitePlayer}
-            blackPlayer={link.blackPlayer}
-            result={link.result}
-          />
-          ))}
+                key={`${link.whitePlayer}-${link.blackPlayer}-${link.result}`} 
+                evaluation={link.evaluation}
+                whitePlayer={link.whitePlayer}
+                blackPlayer={link.blackPlayer}
+                result={link.result}
+                layout={layout}
+            />
         </Box>
-      </Container>
+    ))}
+</Box>
+        </Container>
     </ThemeProvider>
-  );
-}
-
+);
+                }
 export default App;
