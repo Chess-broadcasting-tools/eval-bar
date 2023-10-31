15b0588a5c82cecfbfd454e08b5c83e6bbae9dc2
Rakshit Singh <89170079+Bot-Rakshit@users.noreply.github.com>
2023-10-31 11:41:54 +0530

    Update evalbar.js
    name formatting

--- src/evalbar.js
+++ src/evalbar.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Typography, ButtonGroup, Button } from '@mui/material';
 import './EvalBar.css';
 
-function EvalBar({ evaluation, whitePlayer = 'White', blackPlayer = 'Black', result }) {
-    
+function EvalBar({ evaluation, whitePlayer = 'White', blackPlayer = 'Black', result, layout }) {
     const getBarSegment = (evalValue) => {
         return Math.min(Math.max(Math.round(evalValue), -5), +5);
     };
@@ -18,42 +17,36 @@ function EvalBar({ evaluation, whitePlayer = 'White', blackPlayer = 'Black', res
     const formatName = (name) => {
         const cleanedName = name.replace(/\(.*?\)/g, '').trim();
         const parts = cleanedName.split(' ').filter(part => part.length > 3);
+    
+        const pragPart = parts.find(part => part === "Praggnanandhaa");
+        if (pragPart) {
+            return "Pragg";
+        }
+
         return parts.sort((a, b) => a.length - b.length)[0] || cleanedName.split(' ')[0];
     };
 
     const formatEvaluation = (evalValue) => {
         if (evalValue < -1000 || evalValue > 1000) {
-            return "Mate coming soon";
+            return "Checkmate";
         }
         return evalValue;
     };
 
-    if (result) {
-        return (
-            <Box className="eval-container">
-                <Box className="player-names" display="flex" justifyContent="space-between">
-                    <Typography variant="subtitle1" className="white-player"><b>{formatName(whitePlayer)}</b></Typography>
-                    <Typography variant="subtitle1" className="black-player"><b>{formatName(blackPlayer)}</b></Typography>
-                </Box>
-                <Typography variant="h6" color="white" className="evaluation-value">
-                    {result}
-                </Typography>
-            </Box>
-        );
-    }
-
     return (
-        <Box className="eval-container">
+        <Box className={`eval-container ${layout}`}>
             <Box className="player-names" display="flex" justifyContent="space-between">
-                <Typography variant="subtitle1" className="white-player"><b>{formatName(whitePlayer)}</b></Typography>
-                <Typography variant="subtitle1" className="black-player"><b>{formatName(blackPlayer)}</b></Typography>
+                <Typography variant="h6" className="white-player"><b>{formatName(whitePlayer)}</b></Typography>
+                <Typography variant="h6" className="black-player"><b>{formatName(blackPlayer)}</b></Typography>
             </Box>
             <Typography variant="subtitle1" color="white" className="evaluation-value">
-                {formatEvaluation(evaluation)}
+                {result || formatEvaluation(evaluation)}
             </Typography>
-            <Box className="eval-bars">
-                <Box className="white-bar" style={{ width: getWhiteBarWidth() }}></Box>
-            </Box>
+            {!result && (
+                <Box className="eval-bars">
+                    <Box className="white-bar" style={{ width: getWhiteBarWidth() }}></Box>
+                </Box>
+            )}
         </Box>
     );
 }
