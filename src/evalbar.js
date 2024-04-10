2eaac857e4e6cfc934f6d91c447e5e80ac2e427c
Rakshit Singh <89170079+Bot-Rakshit@users.noreply.github.com>
2024-04-10 23:35:35 +0530

    Update evalbar.js

--- src/evalbar.js
+++ src/evalbar.js
@@ -1,115 +1,152 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Box, Typography } from '@mui/material';
 import './EvalBar.css';
+import blunderSound from './blunder-sound.mp3';
 
-function EvalBar({ evaluation, whitePlayer, blackPlayer, result, layout, customStyles, alert }) {
-    const getBarSegment = (evalValue) => {
-        return Math.min(Math.max(Math.round(evalValue), -5), +5);
-    };
+function EvalBar({ evaluation, whitePlayer, blackPlayer, result, layout, customStyles, alert, onBlunder }) {
+  const prevEvaluationRef = useRef(null);
+  const prevResultRef = useRef(null);
+  const blunderSoundRef = useRef(null);
 
-    const getWhiteBarWidth = () => {
-        if (evaluation >= 99) return '100%';
-        if (evaluation >= 4) return '90%';
-        if (evaluation <= -4) return '10%';
-        return `${50 + getBarSegment(evaluation) * 7.5}%`;
-    };
+  useEffect(() => {
+    if (prevEvaluationRef.current !== null) {
+      const prevEval = prevEvaluationRef.current;
+      const currentEval = evaluation;
 
-    const formatName = (name) => {
-        // Remove commas and other unwanted characters
-        const cleanedName = name.replace(/[,.;]/g, '').trim();
-        const parts = cleanedName.split(' ').filter(part => part.length > 0); // Filter empty parts
-      
-        // Special cases:
-        if (parts.includes("Praggnanandhaa")) { 
-          return "Pragg";
-        }
-        if (parts.includes("Praggnanandhaa,")) { 
-            return "Pragg";
-          }
-        if (parts.includes("Nepomniachtchi,")) { 
-            return "Nepo";
-          }
-          if (parts.includes("Nepomniachtchi")) { 
-            return "Nepo";
-          }
-        if (parts.includes("Warmerdam")) { 
-          return "Max";
-        }
-        if (parts.includes("Goryachkina,")) { 
-            return "Gorya";
-          }
-        if (parts.includes("Goryachkina")) { 
-            return "Gorya";
-          }
-        if (parts.includes("Gukesh")) { 
-            return "Gukesh";
-          }
-        
-        // Find the shortest name
-        let shortestName = parts[0] || ""; // Initialize with empty string
-        for (let i = 1; i < parts.length; i++) {
-          if (parts[i].length < shortestName.length) {
-            shortestName = parts[i];
+      const isBlunder = (prevEval, currentEval) => {
+        if (prevEval >= -4 && prevEval <= 4) {
+          if (
+            (prevEval > 0 && currentEval <= 0) ||
+            (prevEval < 0 && currentEval >= 0) ||
+            Math.abs(currentEval - prevEval) >= 0.6
+          ) {
+            return true;
           }
         }
-      
-        return shortestName;
+        return false;
       };
-      
+
+      if (isBlunder(prevEval, currentEval)) {
+        onBlunder();
+      }
+    }
+    prevEvaluationRef.current = evaluation;
+  }, [evaluation, onBlunder]);
+
+  useEffect(() => {
+    if (prevResultRef.current !== result) {
+      blunderSoundRef.current.play();
+      onBlunder();
+    }
+    prevResultRef.current = result;
+  }, [result, onBlunder]);
+
+  const getBarSegment = (evalValue) => {
+    return Math.min(Math.max(Math.round(evalValue), -5), +5);
+  };
+
+  const getWhiteBarWidth = () => {
+    if (evaluation >= 99) return '100%';
+    if (evaluation >= 4) return '90%';
+    if (evaluation <= -4) return '10%';
+    return `${50 + getBarSegment(evaluation) * 7.5}%`;
+  };
+
+  const formatName = (name) => {
+    // Remove commas and other unwanted characters
+    const cleanedName = name.replace(/[,.;]/g, '').trim();
+    const parts = cleanedName.split(' ').filter(part => part.length > 0); // Filter empty parts
+  
+    // Special cases:
+    if (parts.includes("Praggnanandhaa")) { 
+      return "Pragg";
+    }
+    if (parts.includes("Praggnanandhaa,")) { 
+      return "Pragg";
+    }
+    if (parts.includes("Nepomniachtchi,")) { 
+      return "Nepo";
+    }
+    if (parts.includes("Nepomniachtchi")) { 
+      return "Nepo";
+    }
+    if (parts.includes("Warmerdam")) { 
+      return "Max";
+    }
+    if (parts.includes("Goryachkina,")) { 
+      return "Gorya";
+    }
+    if (parts.includes("Goryachkina")) { 
+      return "Gorya";
+    }
+    if (parts.includes("Gukesh")) { 
+      return "Gukesh";
+    }
     
-    const formatEvaluation = (evalValue) => {
-        if (evalValue < -1000 || evalValue > 1000) {
-            return "Checkmate";
-        }
-        return evalValue;
-    };
+    // Find the shortest name
+    let shortestName = parts[0] || ""; // Initialize with empty string
+    for (let i = 1; i < parts.length; i++) {
+      if (parts[i].length < shortestName.length) {
+        shortestName = parts[i];
+      }
+    }
+  
+    return shortestName;
+  };
+  
+  const formatEvaluation = (evalValue) => {
+    if (evalValue < -1000 || evalValue > 1000) {
+      return "Checkmate";
+    }
+    return evalValue;
+  };
 
-    const displayResult = result !== null ? formatEvaluation(result) : formatEvaluation(evaluation);
-    const evalDisplayClass = result !== null ? "result" : "evaluation-value";
+  const displayResult = result !== null ? formatEvaluation(result) : formatEvaluation(evaluation);
+  const evalDisplayClass = result !== null ? "result" : "evaluation-value";
 
-    return (
-        <Box className={`eval-container ${layout} ${alert ? 'blink-border' : ''}`} 
-             style={{ background: customStyles.evalContainerBg, border: `1px solid ${customStyles.evalContainerBorderColor}` }}>
+  return (
+    <Box className={`eval-container ${layout} ${alert ? 'blink-border' : ''}`} 
+         style={{ background: customStyles.evalContainerBg, border: `1px solid ${customStyles.evalContainerBorderColor}` }}>
+      <Box className="player-names" display="flex" justifyContent="space-between">
+        <Typography 
+          variant="h6" 
+          className="white-player" 
+          style={{ 
+            background: customStyles.whitePlayerColor, 
+            color: customStyles.whitePlayerNameColor, 
+            fontSize: '1.3rem',
+            padding: '1px 13px'
+          }}
+        >
+          <b>{formatName(whitePlayer)}</b>
+        </Typography>
+        <Typography 
+          variant="h6" 
+          className="black-player" 
+          style={{ 
+            background: customStyles.blackPlayerColor, 
+            color: customStyles.blackPlayerNameColor, 
+            fontSize: '1.3rem',
+            padding: '1px 13px'
+          }}
+        >
+          <b>{formatName(blackPlayer)}</b>
+        </Typography>
+      </Box>
 
-            <Box className="player-names" display="flex" justifyContent="space-between">
-                <Typography 
-                    variant="h6" 
-                    className="white-player" 
-                    style={{ 
-                        background: customStyles.whitePlayerColor, 
-                        color: customStyles.whitePlayerNameColor, 
-                        fontSize: '1.3rem',
-                        padding: '1px 13px'
-                    }}
-                >
-                    <b>{formatName(whitePlayer)}</b>
-                </Typography>
-                <Typography 
-                    variant="h6" 
-                    className="black-player" 
-                    style={{ 
-                        background: customStyles.blackPlayerColor, 
-                        color: customStyles.blackPlayerNameColor, 
-                        fontSize: '1.3rem',
-                        padding: '1px 13px'
-                    }}
-                >
-                    <b>{formatName(blackPlayer)}</b>
-                </Typography>
-            </Box>
+      <Typography variant="h7" className={evalDisplayClass} style={{ marginTop: '5px', marginBottom: '5px', fontSize: '18px', fontWeight: 'bold', position: 'absolute', zIndex: 1 }}>
+        {displayResult}
+      </Typography>
 
-            <Typography variant="h7"  className={result ? "result-value" : "evaluation-value"} style={{ marginTop: '5px', marginBottom: '5px', fontSize: '18px', fontWeight: 'bold', position: 'absolute', zIndex: 1 }}>
-    {displayResult}
-</Typography>
-
-            {!result && (
-                <Box className="eval-bars" style={{ height: '30px', borderRadius: '15px', background: customStyles.blackBarColor, overflow: 'hidden', margin: '5px 0', position: 'relative', zIndex: 0 }}>
-                    <Box className="white-bar" style={{ width: getWhiteBarWidth(), background: customStyles.whiteBarColor }}></Box>
-                    <Box className="zero-marker"></Box>
-                </Box>
-            )}
+      {!result && (
+        <Box className="eval-bars" style={{ height: '30px', borderRadius: '15px', background: customStyles.blackBarColor, overflow: 'hidden', margin: '5px 0', position: 'relative', zIndex: 0 }}>
+          <Box className="white-bar" style={{ width: getWhiteBarWidth(), background: customStyles.whiteBarColor }}></Box>
+          <Box className="zero-marker"></Box>
         </Box>
-    );
+      )}
+      <audio ref={blunderSoundRef} src={blunderSound} />
+    </Box>
+  );
 }
 
 export default EvalBar;
